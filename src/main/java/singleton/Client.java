99212f3c947dc1e50ad061f2c601ceca51ffe481
Bhavani Gopikaramanan <bhavanigopika@gmail.com>
2024-07-05 00:26:18 +0530

    Singleton

---
+++ src/main/java/singleton/Client.java
@@ -0,0 +1,23 @@
+package singleton;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Client {
+    public static void main(String[] args) {
+        Lock lock = new ReentrantLock();
+
+        //make a call to the getinstance
+        DatabaseConnection dbc1 = DatabaseConnection.getInstance(lock);
+        DatabaseConnection dbc2 = DatabaseConnection.getInstance(lock);
+
+        DatabaseConnection dbc3 = DatabaseConnection.getInstance();
+        DatabaseConnection dbc4 = DatabaseConnection.getInstance();
+
+        System.out.println(dbc1);
+        System.out.println(dbc2);
+        System.out.println(dbc3);
+        System.out.println(dbc4);
+
+    }
+}
---
+++ src/main/java/singleton/DatabaseConnection.java
@@ -0,0 +1,51 @@
+package singleton;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class DatabaseConnection {
+    private static DatabaseConnection instance = null;
+    Lock lock; //we can use lock object here, otherwise, we can use synchronized keyword in getInstance() method...no need lock in synchronized keyword
+    String dbUrl;
+    String userName;
+    String password;
+
+    //using synchronized keyword
+    public static DatabaseConnection getInstance() {
+        //Double check locking
+        if(instance == null){
+            //instead of lock, we can use synchronized...this also lock it and allow one thread at a time.
+            //I do not want multiple thread entering at the same time
+            //synchronized accept the DatabaseConnection.class since initially instance will be null; -> so, you cannot the synchronize null object (i.e) synchronized(instance)
+            //pass the not null synchronized object that is we synchronize the entire class
+            synchronized(DatabaseConnection.class) {
+                if(instance == null) {
+                    instance = new DatabaseConnection();
+                }
+            }
+        }
+        return instance;
+    }
+
+    //using lock
+    public static DatabaseConnection getInstance(Lock lock) {
+        //Double check locking
+        if(instance == null){
+            //I do not want multiple thread entering at the same time
+
+            lock.lock();
+            if(instance == null) {
+                instance = new DatabaseConnection();
+            }
+            lock.unlock();
+
+        }
+        return instance;
+    }
+
+    //if the manual constructor created, is there default constructor will create? No
+    private DatabaseConnection(){
+
+    }
+
+}

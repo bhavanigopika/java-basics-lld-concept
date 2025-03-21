4338931ded1a1ae087d5b2aedb5e00c7e00bb1a1
Bhavani Gopikaramanan <bhavanigopika@gmail.com>
2025-03-22 00:22:45 +0530

    singleton

---
+++ src/main/java/desingPattern/singleton/DbConnection.java
@@ -0,0 +1,35 @@
+package desingPattern.singleton;
+
+import java.util.concurrent.locks.Lock;
+
+public class DbConnection {
+    private static DbConnection instance = null;
+    Lock lock;
+    String url;
+    String username;
+    String password;
+    private DbConnection(){
+
+    }
+    public static DbConnection getInstance(Lock lock){
+        if(instance == null){
+            lock.lock();
+            if(instance == null){
+                instance = new DbConnection();
+            }
+            lock.unlock();
+        }
+        return instance;
+    }
+
+    public static DbConnection getInstance(){
+        if(instance == null){
+            synchronized(DbConnection.class){
+                if(instance == null){
+                    instance = new DbConnection();
+                }
+            }
+        }
+        return instance;
+    }
+}
